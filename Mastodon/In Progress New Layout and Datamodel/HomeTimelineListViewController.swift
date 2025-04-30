75845fae5cd0d3af1b046b34c04722417f09b3dc
whattherestimefor <shannon@joinmastodon.org>
2025-04-30 14:11:14 -0400

    Start actually loading posts into the new home timeline
    Contributes to IOS-409

--- Mastodon/In Progress New Layout and Datamodel/HomeTimelineListViewController.swift
+++ Mastodon/In Progress New Layout and Datamodel/HomeTimelineListViewController.swift
@@ -1,6 +1,9 @@
 // Copyright © 2025 Mastodon gGmbH. All rights reserved.
 
 import SwiftUI
+import MastodonCore
+import MastodonLocalization
+import Combine
 
 class HomeTimelineListViewController: UIHostingController<HomeTimelineListView>
 {
@@ -11,14 +14,40 @@ class HomeTimelineListViewController: UIHostingController<HomeTimelineListView>
     
     required init?(coder aDecoder: NSCoder) {
         fatalError(
-            "init(coder:) not implemented for NotificationListViewController")
+            "init(coder:) not implemented for HomeTimelineListViewController")
     }
 }
 
 @MainActor
 private class HomeTimelineListViewModel: ObservableObject {
-    @Published var timelineItems = [GenericMastodonPost]()
-    private var feedLoader = TimelineFeedLoader(nil)
+    @Published var timelineItems = [TimelineItem]()
+    private var feedLoader: TimelineFeedLoader?
+    private var feedLoaderResultsSubscription: AnyCancellable?
+    private var feedLoaderErrorSubscription: AnyCancellable?
+    
+    func doInitialLoad() async throws {
+        guard feedLoader == nil else { return }
+        guard let currentUser = AuthenticationServiceProvider.shared.currentActiveUser.value else { assertionFailure("no active authenticated user, cannot create feed loader"); return }
+        feedLoader = TimelineFeedLoader(currentUser: currentUser)
+        feedLoaderResultsSubscription = feedLoader?.$records
+            .sink{ [weak self] results in
+                self?.timelineItems = results.allRecords
+            }
+        // TODO: add feedLoaderErrorSubscription
+        feedLoader?.doFirstLoad()
+    }
+    
+    func requestLoad(_ loadRequest: MastodonFeedLoaderRequest) {
+        guard let feedLoader else { assertionFailure(); return }
+        feedLoader.requestLoad(loadRequest)
+    }
+    
+    func refreshFeedFromTop() async {
+        guard let feedLoader else { assertionFailure(); return }
+        if feedLoader.permissionToLoadImmediately {
+            await feedLoader.loadImmediately(.newer)
+        }
+    }
 }
 
 struct HomeTimelineListView: View {
@@ -29,6 +58,30 @@ struct HomeTimelineListView: View {
     }
     
     var body: some View {
-        Text("HomeTimeline")
+        ScrollViewReader { proxy in
+            List {
+                ForEach(viewModel.timelineItems, id: \.self) { item in // without explicit id, scrollTo(:) does not work
+                    switch item {
+                    case let .missingPosts(newerThan, olderThan, timeGapDescription):
+                        Text(timeGapDescription)
+                    case .post(let post):
+                        let authorInfo = post.metaData.author.displayInfo
+                        Text("Post from \(authorInfo.displayName) \(authorInfo.handle)")
+                    }
+                }
+            }
+            .listStyle(.plain)
+            .refreshable {
+                await viewModel.refreshFeedFromTop()
+            }
+            .accessibilityAction(named: L10n.Common.Controls.Actions.seeMore) {
+                viewModel.requestLoad(.newer)
+            }
+        }
+        .onAppear() {
+            Task {
+                try await viewModel.doInitialLoad()
+            }
+        }
     }
 }
--- Mastodon/In Progress New Layout and Datamodel/TimelineFeedLoader.swift
+++ Mastodon/In Progress New Layout and Datamodel/TimelineFeedLoader.swift
@@ -2,26 +2,54 @@
 
 import Combine
 import Foundation
+import MastodonCore
 import MastodonSDK
 
 enum TimelineItem: Identifiable {
     case post(GenericMastodonPost)
-    case missingPosts(newerThan: (Mastodon.Entity.Status.ID, Date), olderThan: (Mastodon.Entity.Status.ID, Date))
+    case missingPosts(newerThan: Mastodon.Entity.Status.ID, olderThan: Mastodon.Entity.Status.ID, timeGapDescription: String)
     
     var id: String {
         switch self {
         case .post(let post):
             return post.id
-        case .missingPosts(let newerThan, let olderThan):
-            return "\(newerThan)-\(olderThan)"
+        case .missingPosts(let newerThan, let olderThan, let gapDescription):
+            return "\(newerThan)-\(olderThan) (\(gapDescription))"
         }
+    }
+    
+    static func gapBetween(_ olderItem: TimelineItem?, newerItem: TimelineItem?) -> TimelineItem? {
+        switch (olderItem, newerItem) {
+        case (.post(let olderPost), .post(let newerPost)):
+            return .missingPosts(newerThan: olderPost.id, olderThan: newerPost.id, timeGapDescription: olderPost.metaData.createdAt.localizedExtremelyAbbreviatedTimeElapsedUntil(now: newerPost.metaData.createdAt))
+        default:
+            return nil
+        }
+    }
+}
+
+extension TimelineItem: Equatable {
+    static func == (lhs: TimelineItem, rhs: TimelineItem) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+extension TimelineItem: Hashable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
     }
 }
 
 @MainActor
 final class TimelineFeedLoader: MastodonFeedLoader<TimelineItem, CacheableTimeline> {
+    private let authenticatedUser: MastodonAuthenticationBox
     
-    var currentCache: CacheableTimeline?
+    private var currentCache: CacheableTimeline?
+    
+    init(currentUser: MastodonAuthenticationBox) {
+        authenticatedUser = currentUser
+        super.init(nil)
+    }
     
     override func fetchResults(for request: MastodonFeedLoaderRequest) async throws -> CacheableTimeline {
         let olderThan: String?
@@ -29,7 +57,14 @@ final class TimelineFeedLoader: MastodonFeedLoader<TimelineItem, CacheableTimeli
         switch request {
         case .newer:
             olderThan = nil
-            newerThan = records.allRecords.first?.id
+            newerThan = {
+                switch records.allRecords.count {
+                case 0, 1:
+                    return records.allRecords.first?.id
+                default:
+                    return records.allRecords[1].id  // we want to allow the possibility of an overlap in order to detect gaps
+                }
+            }()
         case .older:
             olderThan = records.allRecords.last?.id
             newerThan = nil
@@ -38,8 +73,12 @@ final class TimelineFeedLoader: MastodonFeedLoader<TimelineItem, CacheableTimeli
             newerThan = nil
         }
         
-        assertionFailure("not implemented")
-        return CacheableTimeline(previous: [], statuses: [])
+        await AuthenticationServiceProvider.shared.fetchAccounts(onlyIfItHasBeenAwhile: true) // TODO: legacy comments indicated this may not be the best place for this call
+
+        let response = try await APIService.shared.homeTimeline(sinceID: newerThan, maxID: olderThan, authenticationBox: authenticatedUser)
+        let newCache = CacheableTimeline(older: currentCache?.items ?? [], statuses: response.value)
+        currentCache = newCache
+        return newCache
     }
     
     override func filteredResults(fromCachedType cached: CacheableTimeline) -> [TimelineItem] {
@@ -52,17 +91,71 @@ struct CacheableTimeline: CacheableFeed {
     let items: [TimelineItem]
     
     var filteredPosts: [TimelineItem] {
-    // TODO: if the content is filtered to hide, then nothing that points to it should be included in the filtered results.
-        return items
+        return items.filter { item in
+            switch item {
+            case .missingPosts:
+                return true
+            case .post(let post):
+                if let contentPost = post as? MastodonContentPost {
+                    return !contentPost.content.shouldBeRemovedFromFeed
+                } else if let boost = post as? MastodonBoostPost {
+                    return !boost.boostedPost.content.shouldBeRemovedFromFeed
+                } else {
+                    assertionFailure("unexpected post type")
+                    return true
+                }
+            }
+        }
     }
     
     var hasResults: Bool {
         return !items.isEmpty
     }
  
-    init(previous: [TimelineItem], statuses: [Mastodon.Entity.Status]) {
-        assertionFailure("not implemented")
-        items = []
+    init(older: [TimelineItem], statuses: [Mastodon.Entity.Status]) {
+        let newBatch = statuses.map { status in
+            let post = GenericMastodonPost.fromStatus(status)
+            return TimelineItem.post(post)
+        }
+        
+        var combined: [TimelineItem]
+        
+        let oldestIdInNewBatch = newBatch.last(where: { item in
+            switch item {
+            case .missingPosts: return false
+            case .post: return true
+            }
+        })?.id
+        
+        if let oldestIdInNewBatch {
+            let overlapIndex = older.firstIndex(where: { item in
+                switch item {
+                case .post:
+                    return item.id == oldestIdInNewBatch
+                case .missingPosts:
+                    return false
+                }
+            })
+            if let overlapIndex {
+                let firstOlderIndexToRetain = overlapIndex + 1
+                if firstOlderIndexToRetain < older.count {
+                    let olderTail = older.suffix(from: firstOlderIndexToRetain)
+                    combined = newBatch + olderTail
+                } else {
+                    combined = newBatch
+                }
+            } else if let gapItem = TimelineItem.gapBetween(older.first, newerItem: newBatch.last) {
+                combined = newBatch + [gapItem] + older
+            } else {
+                assert(older.isEmpty, "How else did we get here?")
+                combined = newBatch + older
+            }
+        } else {
+            assert(newBatch.isEmpty, "How else did we get here?")
+            combined = older
+        }
+        
+        items = combined
     }
 }
 
@@ -152,5 +245,17 @@ enum AsyncBool {
         } else {
             return .isFalse
         }
+    }
+}
+
+extension GenericMastodonPost.PostContent {
+    var shouldBeRemovedFromFeed: Bool {
+        guard let filterResults = filtered else { return false }
+        for result in filterResults {
+            if result.filter.filterAction == .hide {
+                return true
+            }
+        }
+        return false
     }
 }
