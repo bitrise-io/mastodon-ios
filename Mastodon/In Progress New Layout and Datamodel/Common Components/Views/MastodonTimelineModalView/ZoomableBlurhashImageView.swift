6de5524288d9a1d2925c53f7126e2caa657dc290
shannon <shannon@joinmastodon.org>
2025-05-13 13:57:39 -0400

    Wrap UIScrollview to get proper zooming behavior

--- Mastodon/In Progress New Layout and Datamodel/Common Components/Views/MastodonTimelineModalView/ZoomableBlurhashImageView.swift
+++ Mastodon/In Progress New Layout and Datamodel/Common Components/Views/MastodonTimelineModalView/ZoomableBlurhashImageView.swift
@@ -3,37 +3,21 @@
 import SwiftUI
 
 struct ZoomableBlurhashImageView: View {
-    @State private var scale: CGFloat = 1.0
-    @State private var lastScale: CGFloat = 1.0
     let image: MastodonImageAttachment
     let viewModel: ImageGalleryViewModel
     let frameSize: CGSize
-    
+   
     var body: some View {
         let originalSize = image.imageDetails.originalSize ?? frameSize
         let aspectRatio = CGFloat(originalSize.height) > 0 ? CGFloat(originalSize.width) / CGFloat(originalSize.height) : 1
         let baseSize = sizeThatFits(aspectRatio: aspectRatio, in: frameSize)
-        let zoomedSize = CGSize(width: baseSize.width * scale, height: baseSize.height * scale)
-        
-            ScrollView([.horizontal, .vertical], showsIndicators: true) {
+        GeometryReader { geo in
+            ZoomableScrollView {
                 BlurhashImageView(imageAttachment: image, viewModel: viewModel)
-                    .frame(
-                        width: zoomedSize.width,
-                        height: zoomedSize.height
-                    )
-                    .gesture(
-                        MagnificationGesture()
-                            .onChanged { value in
-                                let newScale = lastScale * value
-                                scale = max(newScale, 1.0)
-                            }
-                            .onEnded { _ in
-                                lastScale = scale
-                            }
-                    )
+                    .frame(width: baseSize.width, height: baseSize.height) 
             }
-            .ignoresSafeArea()
         }
+    }
     
     private func sizeThatFits(aspectRatio: CGFloat, in container: CGSize) -> CGSize {
         // Fit the image proportionally within the container
