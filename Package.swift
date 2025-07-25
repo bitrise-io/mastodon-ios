5414ff514a9ffb9f154b1b090d7d47fbc6200e5f
ramil-bitrise <ramil.ianbulatov@bitrise.io>
2025-07-25 10:51:49 +0300

    init bazel; install swift dependencies

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "Mastodon-iOS",
+    defaultLocalization: "en",
+    platforms: [
+        .iOS(.v16),
+    ],
+    dependencies: [
+        .package(name: "MastodonSDK", path: "MastodonSDK"),
+        .package(
+            url: "https://github.com/Bearologics/LightChart.git",
+            branch: "master"
+        ),
+        .package(
+            url: "https://github.com/jdg/MBProgressHUD.git",
+            from: "1.2.0"
+        ),
+        .package(
+            url: "https://github.com/tid-kijyun/Kanna.git",
+            from: "5.2.7"
+        ),
+    ]
+)
